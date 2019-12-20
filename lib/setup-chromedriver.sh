f0ba8573f8e69743d17fc0622dc37d6f4d84e84d
Joe Ferrairo <josephferrairo@gmail.com>
2019-12-20 10:49:52 -0500

    set up repo

--- lib/setup-chromedriver.sh
+++ lib/setup-chromedriver.sh
@@ -5,6 +5,8 @@ set -eo pipefail
 version=$1
 arch=$2
 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
 sudo apt-get install -y xvfb screen google-chrome-stable
 
 wget -c -nc --retry-connrefused --tries=0 https://chromedriver.storage.googleapis.com/${version}/chromedriver_${arch}.zip
