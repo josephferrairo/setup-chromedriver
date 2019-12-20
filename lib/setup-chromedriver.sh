5e4c3a422903753e5164efaff279ad508e643ded
Joe Ferrairo <josephferrairo@gmail.com>
2019-12-20 10:43:59 -0500

    Use apt-get

--- lib/setup-chromedriver.sh
+++ lib/setup-chromedriver.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 version=$1
 arch=$2
 
-sudo apt-fast install -y xvfb screen google-chrome-stable
+sudo apt-get install -y xvfb screen google-chrome-stable
 
 wget -c -nc --retry-connrefused --tries=0 https://chromedriver.storage.googleapis.com/${version}/chromedriver_${arch}.zip
 unzip -o -q chromedriver_${arch}.zip
